2fba7611049d14ac0be94eb61976f74e895c916a
Felipe Machado <felipelm123@gmail.com>
2024-06-13 08:46:20 -0500

    init

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.post('/api/logs', (req, res) => {
+  const { level, message, timestamp, platform } = req.body;
+  console.log(`[${timestamp}] [${platform}] [${level}] ${message}`);
+  res.sendStatus(200);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
